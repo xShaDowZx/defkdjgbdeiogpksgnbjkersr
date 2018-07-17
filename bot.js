420eb85f24a582be22582a3f6934ba43ab6984be
NamelessSW <41346517+NamelessSW@users.noreply.github.com>
2018-07-17 23:36:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,58 +1,46 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '*'
+const prefix = '+';
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Nameless Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
 
  client.on('ready',  () => {
+	 client.user.setStatus("Online");
     console.log('By : _xShaDowZx');
     console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
     console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
     console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
   }); 
   
-  client.on('ready', () => {
-     console.log(`----------------`);
-        console.log(`Made By _xShaDowZx - Script By : _xShaDowZx`);
-          console.log(`----------------`);
-        console.log(`ON ${client.guilds.size} Servers '     Script By : _xShaDowZx ' `);
-      console.log(`----------------`);
-    console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setStatus("Online")
-  });
+ 
   client.on("guildCreate", guild => {
     console.log(` Join Bot Of Server ${guild.name} Owner Of Server ${guild.owner.user.username}!`)
   });
+client.on('message', message =>{
 
+let args = message.content.slice(prefix.length).trim().split(' ');
+let cmd = args.shift().toLowerCase();
+
+if (message.author.bot) return;
+if (!message.content.startsWith(prefix)) return;
+try{
+
+ let ops = {
+     active: active
+ }
+    let commandFile = require(`./commands/${cmd}.js`);
+    commandFile.run(client, message, args, ops);
+
+}catch (e) {
+    console.log(e.stack);
+}
+
+});
 //Activity
 client.on('ready',() =>{
 
 setInterval(function(){
 
-client.user.setActivity(` ON ${client.guilds.size}  - *help`,"*")
+client.user.setActivity(` ON ${client.guilds.size} Servers - +help`,"*")
 
 },10000)
 
